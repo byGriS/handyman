953fad8ef1b8e62330c5a985e7f7abcfe90bfa04
GriS <emailofgris@gmail.com>
2020-07-03 16:37:38 +0400

    07.03 fix1

--- public/js/app.js
+++ public/js/app.js
@@ -80030,8 +80030,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
   state: {
     userRole: 0,
     userApi: "",
-    host: "http://handyman.loc/" //host: "http://u0838029.isp.regruhosting.ru/",
-
+    //host: "http://handyman.loc/",
+    host: "http://u0838029.isp.regruhosting.ru/"
   }
 }));
 
