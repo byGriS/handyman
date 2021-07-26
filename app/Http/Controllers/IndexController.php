579364083eb780aa9d62dc07dfed747efbbb51dd
GriS <emailofgris@gmail.com>
2021-07-26 18:51:49 +0400

    tele

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -33,19 +33,27 @@ class IndexController extends Controller {
 
   public function telegram_notification() {
     $telegram = new \Telegram\Bot\Api(config('telegram.bots.mybot.token'));
-    $users = User::where('telegram_chat_id','!=',null)->get();
+    $users = User::where('telegram_chat_id', '!=', null)->get();
     $works = Work::whereStatus(1)->get();
     foreach ($works as $work) {
+      $work = Work::find(22);
       foreach ($work->tasks as $task) {
         $note = $task->notes()->orderBy('dt', 'desc')->first();
         $text = $work->name . PHP_EOL . 'Не указаны данные по работе';
-        if ($note->dt != date("Y-m-d")){
-          foreach($users as $user){
+        if ($note->dt != date("Y-m-d")) {
+          foreach ($users as $user) {
             $telegram->setAsyncRequest(true)->sendMessage(['chat_id' => $user->telegram_chat_id, 'text' => $text]);
           }
-        }else
-          dd("as");
-      };
+        } else {
+          $consumptionStandart = $note->people * $task->standartPeople * $task->standartConsumption;
+          if ($consumptionStandart < $note->consumption) {
+            $text = $work->name . PHP_EOL . 'Не выполнен норматив по раствору' . PHP_EOL . 'Кол-во человек:' . $note->people . ', Кол-во раствора:' . $note->consumption . ', Кол-во раствора по нормативу:' . $consumptionStandart;
+            foreach ($users as $user) {
+              $telegram->setAsyncRequest(true)->sendMessage(['chat_id' => $user->telegram_chat_id, 'text' => $text]);
+            }
+          }
+        }
+      }
     }
 
     /*$telegram = new \Telegram\Bot\Api(config('telegram.bots.mybot.token'));
