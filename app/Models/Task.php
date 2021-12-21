5a78431317ddda67f61a489567683764c7442049
GriS <emailofgris@gmail.com>
2021-12-21 20:04:17 +0400

    test fix

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model {
 
   public function user() {
-    return $this->belongsTo('App\Models\User');
+    return $this->belongsTo('App\Models\User')->withTrashed();
   }
 
   public function work() {
